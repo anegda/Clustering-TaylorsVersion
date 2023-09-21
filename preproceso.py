173ea04ecafda910477e4ef7bf7ab151cd5f5cf3
anegda <anegdaldama@gmail.com>
2023-09-21 12:53:16 +0200

    PERSONALIZED WORDEMBEDDINGS

--- preproceso.py
+++ preproceso.py
@@ -6,6 +6,7 @@ from nltk.tokenize import ToktokTokenizer
 from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
 import gensim.downloader as api
+from gensim.models import Word2Vec
 import numpy as np
 
 def generalCleaning(text):
@@ -45,6 +46,38 @@ def preprocess(dfLyrics):
     model = api.load('word2vec-google-news-300')
     words = set(model.index_to_key)
     dfLyrics['New_Input_vect'] = np.array([np.array([model[i] for i in ls if i in words]) for ls in dfLyrics['Tokens']])
+    text_vect_avg = []
+    for v in dfLyrics['New_Input_vect']:
+        if v.size:
+            text_vect_avg.append(v.mean(axis=0))
+        else:
+            text_vect_avg.append(np.zeros(300, dtype=float))
+    dfLyrics['Lyrics_Embeddings'] = text_vect_avg
+    return dfLyrics
+
+def preprocess_v2(dfLyrics):
+    # 1.- REMOVE SPECIAL CHARACTERS, REMOVE BLANK SPACES, TURN INTO LOWERCASE...
+    dfLyrics["Tokens"] = dfLyrics.Lyrics.apply(generalCleaning)
+
+    # 2.- TOKENIZE
+    tokenizer = ToktokTokenizer()
+    dfLyrics["Tokens"] = dfLyrics.Tokens.apply(tokenizer.tokenize)
+
+    # 3.- REMOVE STOPWORDS, DIGITS AND ONOMATOPEIAS
+    dfLyrics["Tokens"] = dfLyrics.Tokens.apply(specificalCleaning)
+
+    # 4.- LEMMATIZE
+    dfLyrics["Tokens"] = dfLyrics.Tokens.apply(lemmatize)
+
+    # 5.- USE WORDEMBEDDINGS FOR TEXT REPRESENTATION
+    model = Word2Vec(vector_size=300, min_count=10, epochs=100)
+    tokens = [row for row in dfLyrics["Tokens"]]
+    model.build_vocab(tokens)
+    model.train(tokens, total_examples=model.corpus_count, epochs=model.epochs)
+    # model.wv.save_word2vec_format("modelos/Embeddings/my_word_embeddings.txt")
+
+    words = set(model.wv.index_to_key)
+    dfLyrics['New_Input_vect'] = np.array([np.array([model.wv[i] for i in ls if i in words]) for ls in dfLyrics['Tokens']])
     text_vect_avg = []
     for v in dfLyrics['New_Input_vect']:
         if v.size:
